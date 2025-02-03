9b4b465950b356f1de4289f449d028be483a0c6c
Richard Rico Klar <klar.richard@gmail.com>
2025-02-03 08:53:04 +0000

    Literally grabbing stuff from a Snake game to create functional grid lol

---
+++ ProjectAnts.Core/Direction.cs
@@ -0,0 +1,18 @@
+﻿namespace ProjectAnts.Core
+{
+    public class Direction
+    {
+        public readonly static Direction Left = new Direction(0, -1);
+        public readonly static Direction Right = new Direction(0, 1);
+        public readonly static Direction Up = new Direction(-1, 0);
+        public readonly static Direction Down = new Direction(1, 0);
+        public int RowOffset { get; }
+        public int ColumnOffset { get; }
+
+        private Direction(int rowOffset, int columnOffset)
+        {
+            RowOffset = rowOffset;
+            ColumnOffset = columnOffset;
+        }
+    }
+}
